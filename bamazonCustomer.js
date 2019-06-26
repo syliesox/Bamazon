bd80f89265349267297da3d31564a19a5cfbe75c
syliesox <syliesox@gmail.com>
2019-06-26 08:49:18 -0400

    added the displayProducts function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-var connection = mysql.createConnections({
+var connection = mysql.createConnection({
     host: "localhost",
     port: 8889,
     user: "root",
@@ -10,13 +10,27 @@ var connection = mysql.createConnections({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
-    if (err) {
-        console.log("error connecting: " err.stack);
-    }
+connection.connect(function(error) {
+    if (error) throw error;
     displayProducts();
 });
 
 function displayProducts() {
-    
+    connection.query("SELECT * FROM products", function(error, res) {
+        if (error) throw error;
+
+        console.log("============================");
+        console.log("Current Inventory: ");
+        console.log("============================");
+        for (i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id + ", Product Name: " + res[i].product_name + " Department: " + res[i].department_name + " Price: $" + res[i].price + "In Stock: " + res[i].stock_quantity);
+
+            // console.log("Item ID: " + res[i].item_id);
+            // console.log("Product Name: " + res[i].product_name);
+            // console.log("Department: " + res[i].department_name);
+            // console.log("Price: $" + res[i].price);
+            // console.log("In Stock: " + res[i].stock_quantity);
+            // console.log("============================")
+        };
+    })
 }
--- schema.sql
+++ schema.sql
@@ -13,4 +13,17 @@ primary key(item_id)
 
 SELECT * FROM products;
 
-
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+	VALUES ("Scalp Massager Tool", "Personal Care", 7.49, 100),
+	("Stardew Valley", "Video Games", 14.99, 100),
+    ("Adult Coloring Book", "Books", 5.54, 100),
+    ("Jawbone Wireless Head Phones", "Electronics", 99.95, 100),
+    ("Zoodler", "Kitchen", 23.99, 100),
+    ("Hammer", "Tools", 10.99, 100),
+    ("Scarf", "Clothing", 10.99, 100),
+    ("Candles", "Home", 18.70, 100),
+    ("Catan", "Board Games", 43.60, 100),
+    ("Basketball", "Sports", 20.99, 100);
+    
+    
+    
